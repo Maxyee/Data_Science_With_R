d969e71e4021a9628b7684a546b862587138d39c
maxyee <eyaminkhan00@gmail.com>
2019-09-21 00:03:07 +0600

    Added the initial logic about entering Input in R language

---
+++ Entering Input/entering_input.R
@@ -0,0 +1,8 @@
+x <- 1
+print(x)    ## this line prints the value of x equal to 1
+
+msg <- "Hello"
+print(msg)   ## this line prints the value of msg which is Hello
+
+
+x <-  ## incomplete expression
